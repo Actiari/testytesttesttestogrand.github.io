4f903bb656fd9490518cbbd351cc01b04f934a61
Actiari <152329366+Actiari@users.noreply.github.com>
2025-01-31 20:56:27 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,6 @@
+// This is a simple JavaScript file for form validation or other interactions.
+// For now, it just logs a message when the page is loaded.
+
+window.onload = function() {
+    console.log("Simple Website Loaded!");
+};
